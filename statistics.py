8654007761b22507cfb31f75036829dbbf0f0807
FedericoAstolfi <federico.astolfi1@studenti.unimi.it>
2022-06-11 15:34:54 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,6 +1,26 @@
+
 from matplotlib import image
 import matplotlib.pyplot as plt
 import main_torte
 
-main_torte.main(npop = 20, mut_prob = 0.1, ngen =  100, cut_crss = 5, ntorte = 60, grafici = False)
+if __name__ == '__main__':
 
+    crossovers = [i for i in range(0,16)]
+    fig, ax = plt.subplots()
+
+    energie_medie = []
+
+    for j in crossovers:
+
+        energia_media = []
+
+        for i in range(0,100):
+
+            energia_media += [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  100, cut_crss = j, ntorte = 60, grafici = False)]
+
+        energie_medie += sum(energia_media)/len(energia_media)
+
+
+    ax = plt.hist(crossovers, energie_medie)
+
+    plt.show()
