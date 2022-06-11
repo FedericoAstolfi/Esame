72eeee4bc0a30f37e0b6cc57cd797fbf379816f2
FedericoAstolfi <federico.astolfi1@studenti.unimi.it>
2022-06-11 15:10:45 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,6 @@
-import main_torte
+from matplotlib import image
 import matplotlib.pyplot as plt
+import main_torte
+
+main_torte.main(npop = 20, mut_prob = 0.1, ngen =  100, cut_crss = 5, ntorte = 60, grafici = False)
 
