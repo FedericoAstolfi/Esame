ecadf8edb3b51e9d403cbdc66e26908dae4fed7b
TommasoVassura <105877740+TommasoVassura@users.noreply.github.com>
2022-06-12 18:43:55 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -7,11 +7,7 @@ import main_torte
 def cut_ottimo():
 
     crossovers = [i for i in range(0,16)]
-<<<<<<< HEAD
-    fig, (ax1, ax2) = plt.subplots(1,2)
-=======
     #fig, ax = plt.subplots()
->>>>>>> 5b176e285c8ce12b17e09c50911b365cace017b6
 
     energie_medie = []
     goodness_medie = []
@@ -19,21 +15,6 @@ def cut_ottimo():
 
     for j in crossovers:
 
-<<<<<<< HEAD
-        energia_media = [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  10, cut_crss = j, ntorte = 60, grafici = False)[0] for i in range(0,50)]
-        goodness_media = [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  10, cut_crss = j, ntorte = 60, grafici = False)[1] for i in range(0,50)]
-        
-        energie_medie += [sum(energia_media)/len(energia_media)]
-        goodness_medie += [sum(goodness_media)/len(goodness_media)]
-        
-
-
-    ax1.bar(crossovers, energie_medie)
-    ax1.set_title('relazione crossover/energia media')
-    ax2.bar(crossovers, goodness_medie)
-    ax2.set_title('relazione crossover/bontà media')
-    
-=======
         energia_media = []
 
         for i in range(0,10):
@@ -45,7 +26,6 @@ def cut_ottimo():
 
     print(energie_medie)
    # ax.scatter(crossovers, energie_medie)
->>>>>>> 5b176e285c8ce12b17e09c50911b365cace017b6
 
     #plt.show()
 
