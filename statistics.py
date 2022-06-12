ad53838ba55f6549cb0a31fa0c6480d91d1df2f6
FedericoAstolfi <federico.astolfi1@studenti.unimi.it>
2022-06-12 22:20:56 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -2,6 +2,7 @@
 from matplotlib import image
 import matplotlib.pyplot as plt
 import main_torte
+import copy
 
 
 def cut_ottimo():
@@ -15,29 +16,31 @@ def cut_ottimo():
 
     for j in crossovers:
 
-<<<<<<< HEAD
 
         energia_media = [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  10, cut_crss = j, ntorte = 60, grafici = False)[0] for i in range(0,50)]
         goodness_media = [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  10, cut_crss = j, ntorte = 60, grafici = False)[1] for i in range(0,50)]
         
 
-=======
-        energia_media = []
+def correlazione():
 
-        for i in range(0,10):
+    '''studia correlazione tra energia e greed al viariare delle generazioni'''
 
-            energia_media += [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  10, cut_crss = j, ntorte = 80, grafici = False, scritte = True)]
+    energia, greed = main_torte.main(npop = 20, mut_prob = 0.1, ngen = 10000, cut_crss = main_torte.CUT_CRSS, ntorte = 75, grafici = False, scritte = False)
 
-        energie_medie += [sum(energia_media)/len(energia_media)]
+    #normalizzo entrambi i valori di modo che sia più facile vedere che relazione intercorre tra i due
+    energy = copy.copy(energia)
+    bonta = copy.copy(greed)
+    energia = [i/10 for i in energy]
+    greed = [i/65 for i in greed]
 
-
-    print(energie_medie)
-   # ax.scatter(crossovers, energie_medie)
-
-    #plt.show()
-
+    fig, ax = plt.subplots()
+    ax.plot(range(1, len(energia)+1), energia, color ='r', label = 'energia')
+    ax.set_xlabel('generazioni')
+    ax.plot(range(1,len(greed)+1), greed, color = 'g', label = 'greed')
+    ax.legend()
+    plt.show()
 
 if __name__ == '__main__':
+    correlazione()
 
-    cut_ottimo()
->>>>>>> ecadf8edb3b51e9d403cbdc66e26908dae4fed7b
+
