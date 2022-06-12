1171f7fa62ec45461f7fc19f5b9e57d75b7f6d17
FedericoAstolfi <federico.astolfi1@studenti.unimi.it>
2022-06-12 12:15:39 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -6,21 +6,26 @@ import main_torte
 if __name__ == '__main__':
 
     crossovers = [i for i in range(0,16)]
-    fig, ax = plt.subplots()
+    fig, (ax1, ax2) = plt.subplots(1,2)
 
     energie_medie = []
+    goodness_medie = []
+    
 
     for j in crossovers:
 
-        energia_media = []
+        energia_media = [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  10, cut_crss = j, ntorte = 60, grafici = False)[0] for i in range(0,50)]
+        goodness_media = [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  10, cut_crss = j, ntorte = 60, grafici = False)[1] for i in range(0,50)]
+        
+        energie_medie += [sum(energia_media)/len(energia_media)]
+        goodness_medie += [sum(goodness_media)/len(goodness_media)]
+        
 
-        for i in range(0,100):
 
-            energia_media += [main_torte.main(npop = 20, mut_prob = 0.1, ngen =  100, cut_crss = j, ntorte = 60, grafici = False)]
-
-        energie_medie += sum(energia_media)/len(energia_media)
-
-
-    ax = plt.hist(crossovers, energie_medie)
+    ax1.bar(crossovers, energie_medie)
+    ax1.set_title('relazione crossover/energia media')
+    ax2.bar(crossovers, goodness_medie)
+    ax2.set_title('relazione crossover/bontà media')
+    
 
     plt.show()
